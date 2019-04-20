f938d10f3edbbfb457cc04e71a4f70131b4b695a
Udesh Kumarasinghe <mail@udeshuk.me>
2019-04-20 23:40:40 +0530

    UX fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-ScrollReveal().reveal('.member-cell', { delay: 1000 });
+ScrollReveal().reveal('.member-cell', { delay: 200, duration: 1200, easing: 'ease-out' });
+ScrollReveal().reveal('.home-section', { delay: 300, reset: true, duration: 1000 });
 
 var rellax = new Rellax(".rellax", {
   center: true,
